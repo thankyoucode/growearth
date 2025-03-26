b53d8e36139bb39b26d64aa90fbe854cb1d26286
thankyoucode <gauma437@gmail.com>
2025-03-26 18:24:15 +0530

    vercel 3 problem try solution

--- scripts/build_files.sh
+++ scripts/build_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# scripts/build_files.sh
 
 # Install Python dependencies
-pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
